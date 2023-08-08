808c2fd0e9ba3d2b16ae9d4f349aa6ca8f7a6fce
_mad_maran_ <elamaran.infotech@gmail.com>
2023-08-08 15:10:07 +0100

    one last time

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,32 +1,31 @@
 import React from 'react';
 
-const AboutUs = () => {
+const AboutPage = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-pink-500 via-purple-500 to-blue-500">
-      <div className="container mx-auto p-4">
-        <div className="relative rounded-xl bg-opacity-70 backdrop-blur-md bg-white  shadow-lg p-6 md:p-10 max-w-3xl transform scale-100 hover:scale-105 transition-transform duration-300">
-          <h1 className="text-3xl md:text-4xl drop-shadow-md font-semibold mb-6 text-center text-white">
-            About Us
-          </h1>
-          <p className="text-gray-800 mb-8 text-center">
-            We are an AI-based company dedicated to creating innovative
-            solutions for various industries. Our mission is to make AI
-            technology accessible and beneficial to all.
-          </p>
-          <p className="text-gray-800 mb-8">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-            fringilla, quam in malesuada tincidunt, erat quam vestibulum
-            est, eget sollicitudin turpis justo eu nisl. Nullam non velit nec
-            odio fermentum scelerisque. Integer sed leo vel turpis congue
-            auctor vitae non libero.
-          </p>
-          <p className="text-center text-gray-400 text-sm">
-            &copy; {new Date().getFullYear()} Erexa. All rights reserved.
-          </p>
-        </div>
+    <div className="min-h-screen font-Poppins flex items-center justify-center bg-gradient-to-r bg-opacity-90 backdrop-filter backdrop-blur-md p-6 from-blue-100 to-purple-100">
+      <div className="glassmorphism-container shadow-md bg-transparent">
+        <h1 className="text-4xl font-bold text-gray-800 mb-4 drop-shadow-md">About Us</h1>
+        <p className="text-gray-800 mb-4">
+          At <span className="text-purple-800">Erexa</span>, we are passionate about harnessing the power of Artificial Intelligence to transform industries and drive innovation. Our journey began with a vision to create intelligent solutions that empower businesses and individuals alike.
+        </p>
+        <p className="text-gray-800 mb-4">
+          With a team of dedicated AI experts, data scientists, and engineers, we are committed to pushing the boundaries of what AI can achieve. We believe that AI has the potential to revolutionize the way we work, communicate, and solve complex challenges.
+        </p>
+        <p className="text-gray-800 mb-4">
+          Our mission is to deliver cutting-edge AI solutions that streamline processes, enhance decision-making, and provide valuable insights. We specialize in [specific AI domains, e.g., Natural Language Processing, Computer Vision, etc.] and have a proven track record of delivering impactful solutions to our clients.
+        </p>
+        <p className="text-gray-800 mb-4">
+          Beyond technology, our core values center around ethics, transparency, and collaboration. We approach every project with a deep understanding of the responsibility that comes with AI development and deployment.
+        </p>
+        <p className="text-gray-800">
+          Thank you for considering <span className="text-purple-800">Erexa</span> as your AI partner. We are excited about the future and look forward to driving positive change through AI innovation.
+        </p>
+        <p className="mt-8 text-gray-400 text-sm text-center">
+          &copy; {new Date().getFullYear()} <span className="text-purple-800">Erexa</span>. All rights reserved.
+        </p>
       </div>
     </div>
   );
 };
 
-export default AboutUs;
+export default AboutPage;
